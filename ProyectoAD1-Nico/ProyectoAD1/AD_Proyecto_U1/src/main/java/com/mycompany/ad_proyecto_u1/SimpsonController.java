cbefdc8ef9ef1c8b78eb8a13d843d02f3d51d2f4
Nicosanju <a24nicolassc@iesantonlosada.gal>
2025-11-10 14:58:02 +0100

    Add files via upload
    Creada la interfaz sin la accion de botones

---
+++ ProyectoAD1-Nico/ProyectoAD1/AD_Proyecto_U1/src/main/java/com/mycompany/ad_proyecto_u1/SimpsonController.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.ad_proyecto_u1;
+
+
+public class SimpsonController {
+    
+    private SimpsonFrame view;
+
+    public SimpsonController(SimpsonFrame view) {
+        this.view = view;
+    }
+    
+    
+    
+    
+}
