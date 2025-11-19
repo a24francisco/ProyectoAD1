b246d411412634f20f3e35ec595c3f5588c3480e
a24francisco <a24franciscoaa@iesantonlosada.gal>
2025-11-19 14:58:37 +0100

    Solo falta dividir en paquetes

---
+++ AD_Proyecto_U1/src/main/java/com/mycompany/ad_proyecto_u1/Usuario.java
@@ -0,0 +1,201 @@
+package com.mycompany.ad_proyecto_u1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Roi
+ */
+public class Usuario {
+
+    private String nombre;
+    private String contraseña;
+
+    public List<Personaje> buscados;
+    public List<Personaje> total;
+    private SimpsonFrame view;
+
+    public Usuario(String nombre, String contraseña) throws IOException {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+        this.buscados = new ArrayList<>();
+        this.total = UtilidadesGson.leerApi();
+        DesserializarLista();
+        DesserializarBusqueda();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public void mostrarListaBuscados() {
+        for (Personaje p : buscados) {
+            System.out.println(p);
+        }
+    }
+
+    public void mostrarListaTotal() {
+        for (Personaje p : total) {
+            System.out.println(p);
+        }
+    }
+
+    private void SerializarBusqueda() {
+
+        try {
+            FileOutputStream fos = new FileOutputStream(nombre + ".src");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(buscados);
+            oos.close();
+            fos.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private List DesserializarBusqueda() {
+        File file = new File(nombre + ".src");
+        if (file.exists()) {
+            try (FileInputStream fis = new FileInputStream(nombre + ".src"); ObjectInputStream ois = new ObjectInputStream(fis);) {
+
+                this.buscados = (ArrayList<Personaje>) ois.readObject();
+
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            } catch (ClassNotFoundException c) {
+                System.out.println("Class not found");
+                c.printStackTrace();
+            }
+            return buscados;
+        }
+        return null;
+
+    }
+
+    private void SerializarLista() {
+        try {
+            FileOutputStream fos = new FileOutputStream(nombre + ".list");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(total);
+            oos.close();
+            fos.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private List DesserializarLista() {
+        File file = new File(nombre + ".list");
+        
+        if (file.exists()) {
+            try (FileInputStream fis = new FileInputStream(nombre + ".list"); ObjectInputStream ois = new ObjectInputStream(fis);) {
+
+                this.total = (ArrayList<Personaje>) ois.readObject();
+
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            } catch (ClassNotFoundException c) {
+                System.out.println("Class not found");
+                c.printStackTrace();
+            }
+            return total;
+        }
+        return null;
+
+    }
+
+    public boolean crearPersonaje(Personaje p) {
+
+        if (p == null) {
+            return false;
+        }
+
+        int contador = 0;
+        for (int i = 0; i < total.size(); i++) {
+            if (total.get(i).getId() == p.getId()) {
+                contador++;
+            }
+        }
+        if (contador > 0) {
+            System.out.println("ID ya existente");
+           
+            return false;
+        } else {
+            total.add(p);
+            SerializarLista();
+            return true;
+        }
+    }
+
+    public Personaje filtrarNombre(String nombre) {
+
+        int contador = 0;
+        Personaje x = new Personaje();
+        nombre = nombre.toUpperCase();
+
+        for (Personaje p : total) {
+            if (nombre.equals(p.getName().toUpperCase())) {
+                x = p;
+                contador++;
+            }
+        }
+
+        if (contador > 0) {
+
+            buscados.add(x);
+            SerializarBusqueda();
+            return x;
+
+        } else {
+            System.out.println("Usuario no encontrado");
+            JOptionPane.showMessageDialog(view, "Este nombre no existe o está mal escrito");
+            return null;
+        }
+    }
+
+    public void delUsuario(String nombre) {
+        int contador = 0;
+        nombre = nombre.toUpperCase();
+        Personaje x = new Personaje();
+        for (Personaje per : total) {
+            if (per.getName().toUpperCase().equals(nombre)) {
+                x = per;
+                contador++;
+            }
+        }
+        if (contador > 0) {
+            
+            total.remove(x);
+            SerializarLista();
+        }
+        
+    }
+}
---
+++ AD_Proyecto_U1/src/main/java/com/mycompany/ad_proyecto_u1/crearPersonajeController.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ad_proyecto_u1;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dam2_alu11@inf.ald
+ */
+public class crearPersonajeController {
+    
+    crearPersonajeJDialog view;
+    Usuario u;
+    Registro r= new Registro();
+
+    public crearPersonajeController(crearPersonajeJDialog view,Usuario u) {
+    
+    this.view = view;
+    this.u=u;
+    this.view.guardarButtonACtionListener(this.getGuardarButton());
+    this.view.cancelarButtonACtionListener(this.getCancelarButton());
+    
+    
+    
+    }
+    private ActionListener getGuardarButton(){
+    
+        ActionListener al = new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+              try{
+                String nombre = view.getName();
+                int id = view.getId();
+                String trabajo = view.getTrabajo();
+                String cumpleaños = view.getCumpleaños();
+                int edad = view.getEdad();
+                String frase = view.getPhrases();
+                String genero =view.getGenero();
+                  if (nombre.isEmpty()) {
+                      JOptionPane.showMessageDialog(view, "Complete el nombre");
+                      return;
+                  }
+               Personaje p = Personaje.crearPersonaje(id,edad,cumpleaños,genero,nombre,null,List.of(frase),trabajo,null);
+                  if (u.crearPersonaje(p)) {
+                      JOptionPane.showMessageDialog(view, "Personaje creado correctamente");
+                      view.dispose();
+                  }
+                  else{
+                       JOptionPane.showMessageDialog(view, "El id ya existe");
+                  }
+                    
+                
+              }
+             catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(view, "La edad o el id no puede contener letras");
+                    }
+                }
+               
+               
+            
+    
+    };
+        return al;
+}
+    
+    public ActionListener getCancelarButton(){
+        ActionListener al= new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                view.dispose();
+            }
+        };
+        return al;
+    }
+}
